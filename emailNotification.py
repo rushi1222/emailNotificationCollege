9e0110f278735460f968c548f22ab7773e326085
rushi1222 <k.rushi1222@gmail.com>
2023-11-04 01:51:43 -0400

    6th push

--- emailNotification.py
+++ emailNotification.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import time
 import hashlib
 import smtplib
 from email.message import EmailMessage
@@ -10,7 +9,6 @@ from email.message import EmailMessage
 # Configuration
 url = 'https://secure6.saashr.com/ta/6000630.careers?CareersSearch='
 div_class = 'c-jobs-list'  # Replace with actual class if different
-check_interval = 30  # Time between checks in seconds
 previous_hash = None
 browser = webdriver.Firefox()
 
@@ -24,7 +22,7 @@ def send_notification(body):
     # Configure the SMTP settings for your email server
     server = smtplib.SMTP('smtp.gmail.com', 587)  # 587 is typically used for starttls
     server.starttls()
-    server.login('k.rushi15108@gmail.com', 'qtwk vicf cfua obpl')
+    server.login('k.rushi15108@gmail.com', 'qtwk vicf cfua obpl')  # Replace with actual credentials
     server.send_message(msg)
     server.quit()
 
@@ -35,39 +33,28 @@ def check_div_change():
     try:
         # Find the div by class name
         WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "c-jobs-list"))
+            EC.presence_of_element_located((By.CLASS_NAME, div_class))
         )
-        div_content = browser.find_element(By.CLASS_NAME, "c-jobs-list").get_attribute('innerHTML')
+        div_content = browser.find_element(By.CLASS_NAME, div_class).get_attribute('innerHTML')
 
         # Calculate the current hash
         current_hash = hashlib.md5(div_content.encode('utf-8')).hexdigest()
 
         # Check if the div has changed
-        if previous_hash is not None:
-            if current_hash != previous_hash:
-                send_notification(f"The div has changed.\n\n{div_content}")
-            else:
-                # Send a notification that the div has not changed only for testing purposes
-                # Comment out or remove the next line in production
-                send_notification("The div has not changed.")
+        if previous_hash is not None and current_hash != previous_hash:
+            send_notification(f"The div has changed.\n\n{div_content}")
         else:
-            # This is the first time we're checking; don't notify yet
+            # This is the first time we're checking or the div has not changed; don't notify
             pass
-        
+
         # Update the previous hash
         previous_hash = current_hash
+
     except Exception as e:
         print(f"An error occurred: {e}")  # Print the exception
 
-try:
-    while True:
-        check_div_change()
-        time.sleep(check_interval)
-except KeyboardInterrupt:
-    print("Stopped by the user.")
-finally:
-    browser.quit()
+    finally:
+        browser.quit()
 
 if __name__ == "__main__":
     check_div_change()
-    browser.quit()
